54b8d4ba3ee3507fbd0af9fd1976026d6d279558
Debtanu <debtanum@ceragon.com>
2025-07-25 00:04:24 +0530

    Add initial React Native frontend

---
+++ mobile/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { SafeAreaView, Text, StyleSheet } from 'react-native';
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.text}>Hello from Devices App!</Text>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 20,
+  },
+});
